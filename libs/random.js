2bbffcd37de17c4db54c39e894d91dfe2f0f3437
iamjoel <iamjoel007@gmail.com>
2014-02-01 15:35:57 +0800

    make random number with range

--- libs/random.js
+++ libs/random.js
@@ -1,4 +1,6 @@
 (function  (ctx) {
+    ctx.tool || (ctx.tool = {});
+    var tool = ctx.tool; 
     var UPPERCASE_ALPHA = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
     var LOWER_ALPHA = 'abcdefghijklmnopqrstuvwxyz';
     var ALL_ALPHA = LOWER_ALPHA + UPPERCASE_ALPHA;
@@ -8,10 +10,11 @@
 
     var random = Math.random; 
 
+
     /*
     * 生成随机字母
     */
-    ctx.makeRandomAlpha = function(alphaNum, option) {
+    tool.makeRandomAlpha = function(alphaNum, option) {
         alphaNum = isNaN(alphaNum) ? 1 : parseInt(alphaNum);
         alphaNum = alphaNum <= 0 ? 1 : alphaNum;
         var type = TYPE_LOWER_ALPHA_ONLY;
@@ -23,6 +26,39 @@
             strArr.push(makeOneRandomAlpha(type));
         }
         return strArr.join('');
+
+    };
+
+    /*
+    * 随机取数组中的某个元素
+    */
+    tool.randomItemInArr = function(arr){
+        if(!isArray(arr)) {
+            console.error('param@arr: %s shoule be array',arr);
+            throw new Error('param@arr shoule be array');
+        }
+        var len = arr.length;
+        var randomIndex = parseInt(len * random(), 10);
+        return arr[randomIndex];
+    };
+
+     /*
+    * 随机取数组中的某个元素 [min,max)
+    */
+    tool.randomNum = function(min, max, beInteger) {
+        min = min || 0;
+        max = max || 99999999;
+        beInteger = beInteger !== undefined ? beInteger : true; 
+        if(min > max) {
+            var temp = min;
+            min = max;
+            max = temp;
+        }
+        var result = min + (max - min) * random();
+        if(beInteger){
+            result = Math.floor(result);
+        }
+        return result;
 
     };
     
@@ -41,18 +77,10 @@
             default :
                 alphas = LOWER_ALPHA;
         }
-        return ctx.randomItemInArr(alphas.split(''));
+        return tool.randomItemInArr(alphas.split(''));
     };
 
-    ctx.randomItemInArr = function(arr){
-        if(!isArray(arr)) {
-            console.error('param@arr: %s shoule be array',arr);
-            throw new Error('param@arr shoule be array');
-        }
-        var len = arr.length;
-        var randomIndex = parseInt(len * random(), 10);
-        return arr[randomIndex];
-    };
+    
 
     function isArray (arr) {
         return toString.call(arr) === '[object Array]';
--- test/test-random.js
+++ test/test-random.js
@@ -1,5 +1,5 @@
 chai.Should();
-var ctx = window;
+var tool = window.tool;
 var isAlpha = function(str) {
     return /^[a-zA-Z]+$/g.test(str);
 }
@@ -9,22 +9,25 @@ var isUpperAlpha = function(str) {
 var isLowerAlpha = function(str) {
     return /^[a-z]+$/g.test(str);
 }
+var isInt = function(str) {
+   return /^\d+$/g.test(str); 
+}
 describe('random', function() {
     describe('#makeRandomAlpha', function() {
         it('alpha different cases', function() {
             for (var i = 0; i < 10; i++) {
-                isLowerAlpha(ctx.makeRandomAlpha()).should.be.true;
+                isLowerAlpha(tool.makeRandomAlpha()).should.be.true;
             }
             for (var i = 0; i < 10; i++) {
                 isUpperAlpha(
-                    ctx.makeRandomAlpha(1, {
+                    tool.makeRandomAlpha(1, {
                         allowUpperCase: true
                     })
                 ).should.be.true;
             }
             for (var i = 0; i < 10; i++) {
                 isAlpha(
-                    ctx.makeRandomAlpha(1, {
+                    tool.makeRandomAlpha(1, {
                         allowAll: true
                     })
                 ).should.be.true;
@@ -33,25 +36,47 @@ describe('random', function() {
         });
 
         it('alpha length', function() {
-            ctx.makeRandomAlpha().split('').length.should.equal(1); // use split because Ie6 not support String.prototype.length
-            ctx.makeRandomAlpha(-1).split('').length.should.equal(1);
-            ctx.makeRandomAlpha(0).split('').length.should.equal(1);
-            ctx.makeRandomAlpha(10).split('').length.should.equal(10);
+            tool.makeRandomAlpha().split('').length.should.equal(1); // use split because Ie6 not support String.prototype.length
+            tool.makeRandomAlpha(-1).split('').length.should.equal(1);
+            tool.makeRandomAlpha(0).split('').length.should.equal(1);
+            tool.makeRandomAlpha(10).split('').length.should.equal(10);
         });
     });
 
-    describe('#randomItemInArr',function(){
+    describe('#randomItemInArr', function(){
         it('item in arr', function(){
             var arr = [1,2,3,4];
             for(var i = 0; i < 10; i++) {
-                arr.indexOf(ctx.randomItemInArr(arr)).should.greaterThan(-1);
+                arr.indexOf(tool.randomItemInArr(arr)).should.greaterThan(-1);
             }
         });
 
         it('param error', function(){
             var obj = {};
-            ctx.randomItemInArr.bind(obj).should.throw();
+            tool.randomItemInArr.bind(obj).should.throw();
         });
 
+    });
+
+    describe('#randomNum', function(){
+        it('number range', function(){
+            for(var i= 0; i < 10; i++) {
+                tool.randomNum().should.greaterThan(-1);
+            }
+            for(var i= 0; i < 10; i++) {
+                tool.randomNum(5,100).should.greaterThan(4).lessThan(100);
+            }
+        });
+
+        it('random integer', function(){
+           for(var i= 0; i < 10; i++) {
+                isInt(tool.randomNum(0, 100)).should.be.true;
+            } 
+        });
+        it('random float', function(){
+           for(var i= 0; i < 10; i++) {
+                isInt(tool.randomNum(0, 100, false)).should.be.false;
+            } 
+        });
     });
 });
