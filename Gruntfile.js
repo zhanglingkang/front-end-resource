b4da2f0014e016a32a8def3cbc378b8427f30920
iamjoel <iamjoel007@gmail.com>
2014-02-02 15:17:45 +0800

    add jsdoc

--- Gruntfile.js
+++ Gruntfile.js
@@ -14,12 +14,19 @@
 
       }
     }
+    , jsdoc : {
+      src : ['libs/**/*.js']
+      , options: {
+        destination: 'doc'
+      }
+    }
   });
 
 
   // 加载所有的plugin，so easy!
   require('matchdep').filterDev('grunt-*').forEach(grunt.loadNpmTasks);
 
+  grunt.registerTask('doc', ['jsdoc']);
   grunt.registerTask('build', ['jshint']);
 
   grunt.registerTask('default', ['build']);
