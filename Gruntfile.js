b8986773ee472174ca297939801168d162f8bd3e
iamjoel <iamjoel007@gmail.com>
2014-02-02 13:30:52 +0800

    add jshint

---
+++ Gruntfile.js
@@ -0,0 +1,26 @@
+﻿module.exports = function(grunt) {
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json')
+    , jshint: {
+      all: {
+        options: {
+          jshintrc: true
+        }
+        , src: [
+          'libs/**/*.js'
+          , 'test/**/*.js'
+          , '!test/vendor/*.js'
+        ]
+
+      }
+    }
+  });
+
+
+  // 加载所有的plugin，so easy!
+  require('matchdep').filterDev('grunt-*').forEach(grunt.loadNpmTasks);
+
+  grunt.registerTask('build', ['jshint']);
+
+  grunt.registerTask('default', ['build']);
+};
---
+++ tools/jshint.md
@@ -0,0 +1,8 @@
+## jshint
+### 默认值
+https://github.com/jshint/jshint/blob/master/examples/.jshintrc
+
+### 更多资源
+* [jshint github](https://github.com/jshint/jshint)
+* [jshint offical site](http://jshint.com)
+* [jshint options reference](http://www.jshint.com/docs/options/)
