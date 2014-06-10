25e52cf12c8d2c2204be82cec921d318401c7b25
joel <iamjoel007@gmail.com>
2014-06-10 11:18:36 +0800

    增加可以产看demo的任务

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,87 +1,119 @@
 module.exports = function(grunt) {
-  grunt.initConfig({
-    pkg: grunt.file.readJSON('package.json')
-    , connect: {
-      test: {
-        port: 3001
-        , base: '.'
-      }
-    }
-    , mocha: {
-      'test-lib' :{// 自己写的脚本
-        options: {
-          urls: [ 'localhost:3001/test/test-random.html' ]
-          , options: {
-            timeout: 10000
-          }
-          , run: true
-
-          // src: [ 'test/*.html' , '!test/*.html']
-        }
-      }
-      // , 'test-lib' :{// 学习第三方库写的测试脚本
-      //   options: {
-      //     // urls: [ 'localhost:3001/test/*.html' ]
-      //     urls: [ 'test/*.html' ]
-      //   }
-      // }
-    }
-    , jshint: {
-      all: {
-        options: {
-          jshintrc: true
-        }
-        , src: [
-          'libs/**/*.js'
-          , '!libs/vendor/*.js'
-          , 'test/**/*.js'
-          , '!test/vendor/*.js'
-        ]
-
-      }
-    }
-    , jsdoc : {
-      src : ['my-lib/*/*.js','!my-lib/doc/**/*.js']
-      , options: {
-        destination: 'my-lib/doc'
-      }
-    }
-    , bowercopy: {
-        options: {
-            // srcPrefix: 'bower_components',// default find in in .bowerrc
-            //clean: false // default false
-        },
-        libs: {
-            options: {
-                destPrefix: 'libs/vendor'
-            },
-            files: {
-              'requirejs.js': 'requirejs/require.js'
-              ,'jquery.min.js': 'jquery/dist/jquery.min.js'
-              ,'lodash.min.js': 'lodash/dist/lodash.min.js'
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        'connect': {
+            test: {
+                port: 3001,
+                base: '.'
             }
         },
-        test: {
+        'mocha': {
+            'test-lib': { // 自己写的脚本
+                options: {
+                    urls: ['localhost:3001/test/test-random.html'],
+                    options: {
+                        timeout: 10000
+                    },
+                    run: true
+
+                    // src: [ 'test/*.html' , '!test/*.html']
+                }
+            }
+            // , 'test-lib' :{// 学习第三方库写的测试脚本
+            //   options: {
+            //     // urls: [ 'localhost:3001/test/*.html' ]
+            //     urls: [ 'test/*.html' ]
+            //   }
+            // }
+        },
+        'jshint': {
+            all: {
+                options: {
+                    jshintrc: true
+                },
+                src: [
+                    'libs/**/*.js', '!libs/vendor/*.js', 'test/**/*.js', '!test/vendor/*.js'
+                ]
+
+            }
+        },
+        'jsdoc': {
+            src: ['my-lib/*/*.js', '!my-lib/doc/**/*.js'],
             options: {
-                destPrefix: 'test/vendor'
+                destination: 'my-lib/doc'
+            }
+        },
+        'clean':{
+            files: ['.grunt','build']
+        },
+        'copy': {
+            'vendor':{
+              'src': ['vendor/**/*'],
+              'dest': 'build/'
             },
-            files: {
-              'mocha.js': 'mocha/mocha.js'
-              ,'mocha.css': 'mocha/mocha.css'
-              ,'chai.js': 'chai/chai.js'
-              ,'sinon.js': 'sinonjs/sinon.js'
+            'my-lib': {
+                'src': [
+                    'my-lib/**/*.html',
+                    'my-lib/**/*.css',
+                    'my-lib/**/*.js',
+                    '!**/node_modules/**'
+                ],
+                'dest': 'build/'
+            },
+            'learn': {
+                'src': [
+                    'learn/**/*.html',
+                    'learn/**/*.css',
+                    'learn/**/*.js',
+                    '!**/node_modules/**'
+                ],
+                'dest': 'build/'
+            }
+        },
+        'gh-pages': {
+            options: {
+                base: 'build'
+            },
+            src: ['**']
+        },
+        'bowercopy': {
+            options: {
+                // srcPrefix: 'bower_components',// default find in in .bowerrc
+                //clean: false // default false
+            },
+            libs: {
+                options: {
+                    destPrefix: 'libs/vendor'
+                },
+                files: {
+                    'requirejs.js': 'requirejs/require.js',
+                    'jquery.min.js': 'jquery/dist/jquery.min.js',
+                    'lodash.min.js': 'lodash/dist/lodash.min.js'
+                }
+            },
+            test: {
+                options: {
+                    destPrefix: 'test/vendor'
+                },
+                files: {
+                    'mocha.js': 'mocha/mocha.js',
+                    'mocha.css': 'mocha/mocha.css',
+                    'chai.js': 'chai/chai.js',
+                    'sinon.js': 'sinonjs/sinon.js'
+                }
             }
         }
-      }
-  });
+    });
 
 
-  // Load grunt tasks from NPM packages
-  require( "load-grunt-tasks" )( grunt );
+    // Load grunt tasks from NPM packages
+    require("load-grunt-tasks")(grunt);
 
-  grunt.registerTask('doc', ['jsdoc']);
-  grunt.registerTask('bower', 'bowercopy');// alias for bowercopy
-  grunt.registerTask('build', ['jshint','bower']);
+    grunt.registerTask('doc', ['jsdoc']);
+    grunt.registerTask('bower', 'bowercopy'); // alias for bowercopy
+    grunt.registerTask('build', ['jshint', 'bower']);
+    grunt.registerTask('pre', ['clean','copy']);
+    grunt.registerTask('publish', ['pre','gh-pages']);
 
-  grunt.registerTask('default', ['build']);
+    grunt.registerTask('default', ['build']);
 };
